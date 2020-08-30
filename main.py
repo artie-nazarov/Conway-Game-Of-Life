3f070227bc59d5fe14e68e7588f6a30c219d1126
Artie Nazarov <artienazarov@gmail.com>
2020-08-30 12:08:25 -0700

    set up pygame env

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+width, height = 1920, 1080 # Screen size
+size = (width, height)
+
+pygame.init()
+pygame.display.set_caption("Conway's Game of Life")
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+black = (0, 0, 0)
+blue = (0, 14, ,71)
+white = (255, 255, 255)
